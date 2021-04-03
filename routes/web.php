045455c86aa6466a3ab533325a54e728c86695cf
gembatplc <gembatplc@github.com>
2021-04-03 16:17:41 +0600

    added new route

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home',
+ [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+ Route::get('/test',function(){
+     return "testing perpose";
+ });
